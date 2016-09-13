041c302ee7bc0d0fb2daaa8fb2303bab046f2ff4
Jim Kang <jimkang@gmail.com>
2016-09-13 11:00:59 -0400

    Add display problem rendering. Try fonts.

---
+++ render-display-problem.js
@@ -0,0 +1,36 @@
+var d3 = require('d3-selection');
+var accessor = require('accessor');
+
+var getId = accessor();
+
+function renderDisplayProblem({problem, setRoute}) {
+  var displaySection = d3.select('#display-problem');
+  displaySection.classed('hidden', false);
+
+  displaySection.select('.edit-button').on('click', edit);
+
+  displaySection.select('.problem .problem-text').datum(problem)
+    .text(problem.problemText);
+
+  var choiceRoot = displaySection.select('.choice-root');
+  var choices = choiceRoot.selectAll('.choice').data(problem.choices, getId);
+
+  choices.exit().remove();
+
+  var newChoices = choices.enter().append('li').classed('choice', true);
+  newChoices.append('div').classed('presenter', true).append('img');
+  newChoices.append('div')
+    .classed('choice-text', true);
+
+  var updateChoices = newChoices.merge(choices);
+  updateChoices.attr('id', getId);
+  updateChoices.selectAll('.presenter img').attr('src', accessor('presenterImageURL'));
+  updateChoices.selectAll('.choice-text').text(accessor('text'));
+
+  function edit() {
+    displaySection.classed('hidden', true);
+    setRoute('/problem/' + problem.id + '/edit');
+  }
+}
+
+module.exports = renderDisplayProblem;
