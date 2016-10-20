5be368f53af5e7845168f1b17d2171f6819b6e67
Jim Kang <jimkang@gmail.com>
2016-10-19 20:53:00 -0400

    Avoid re-creating tear elements on rerender. Make problem text readable.

--- render-display-problem.js
+++ render-display-problem.js
@@ -11,14 +11,11 @@ function renderDisplayProblem({problem, setRoute}) {
   d3.selectAll('body > section:not(#display-problem)').classed('hidden', true);
 
   d3.select('.change-council-button').on('click', changeCouncil);
-// TODO: Rerender everything to avoid artifacts.
+
   var displaySection = d3.select('#display-problem');
   displaySection.classed('hidden', false);
 
   displaySection.select('.edit-button').on('click', edit);
-
-  displaySection.select('.problem .problem-text').datum(problem)
-    .text(problem.text);
 
   var choiceRoot = displaySection.select('.choice-root');
   var choices = choiceRoot.selectAll('.choice').data(problem.choices, getId);
@@ -27,14 +24,17 @@ function renderDisplayProblem({problem, setRoute}) {
 
   var newChoices = choices.enter().append('li').classed('choice', true);
   newChoices.append('div').classed('presenter', true).append('img');
-  var textContainers = newChoices
-    .append('div').classed('dialogue-text-container', true);
-  createTextContainerContents(textContainers);
+  var choiceTextContainers =
+    newChoices.append('div').classed('dialogue-text-container', true);
+  renderTextContainerContents(choiceTextContainers);
 
   var updateChoices = newChoices.merge(choices);
   updateChoices.attr('id', getId);
   updateChoices.selectAll('.presenter img').attr('src', accessor('presenterImageURL'));
   updateChoices.selectAll('.dialogue-text').text(accessor('text'));
+
+  renderTextContainerContents(displaySection.select('.problem .dialogue-text-container'));
+  displaySection.select('.problem .dialogue-text').text(problem.text);  
 
   function edit() {
     displaySection.classed('hidden', true);
@@ -42,7 +42,7 @@ function renderDisplayProblem({problem, setRoute}) {
   }
 
   function changeCouncil() {
-    getCouncil({numberOfMembers: 5}, sb(updateCouncil, handleError));
+    getCouncil({numberOfMembers: problem.choices.length}, sb(updateCouncil, handleError));
   }
 
   function updateCouncil(members) {
@@ -55,35 +55,44 @@ function renderDisplayProblem({problem, setRoute}) {
 }
 
 // TODO: Do you really need a unique tear for each dialogue-text?
-function createTextContainerContents(textContainers) {
-  textContainers.append('div').classed('dialogue-text', true);
+function renderTextContainerContents(textContainers) {
+  ensureExists(textContainers, 'div', 'dialogue-text');
+  
   drawTear({
-    svg: textContainers.append('svg').classed('top-paper-edge', true),
+    svg: ensureExists(textContainers, 'svg', 'top-paper-edge'),
     direction: [0, -1],
     length: 1500,
     maxThickness: 5
   });
 
   drawTear({
-    svg: textContainers.append('svg').classed('bottom-paper-edge', true),
+    svg: ensureExists(textContainers, 'svg', 'bottom-paper-edge'),
     direction: [0, 1],
     length: 1500,
     maxThickness: 5
   });
 
   drawTear({
-    svg: textContainers.append('svg').classed('left-paper-edge', true),
+    svg: ensureExists(textContainers, 'svg', 'left-paper-edge'),
     direction: [-1, 0],
     length: 1000,
     maxThickness: 10
   });
 
   drawTear({
-    svg: textContainers.append('svg').classed('right-paper-edge', true),
+    svg: ensureExists(textContainers, 'svg', 'right-paper-edge'),
     direction: [1, 0],
     length: 1000,
     maxThickness: 10
   });
+}
+
+function ensureExists(parentSelection, elementType, className) {
+  var existing = parentSelection.select('.' + className);
+  if (existing.empty()) {
+    existing = parentSelection.append(elementType).classed(className, true);
+  }
+  return existing;
 }
 
 module.exports = renderDisplayProblem;
